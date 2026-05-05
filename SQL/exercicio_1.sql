c5b8c1ab77881be137fe5389b607886e27fcdca5
Marina Canuto <maricanuto01.pt@gmail.com>
2026-05-05 09:15:58 -0300

    Adiciona exercicio 1 SQL
    Added SQL queries to list films, actors, and their categories.

--- SQL/exercicio_1.sql
+++ SQL/exercicio_1.sql
@@ -1,4 +1,35 @@
--- Exercícios SQL
+-- 1. Lista de filmes e suas categorias
 
--- Estrutura criada conforme solicitado na atividade.
--- As consultas principais foram realizadas em NoSQL (MongoDB).
+SELECT 
+    f.title AS filme,
+    c.name AS categoria
+FROM film f
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+ORDER BY f.title;
+
+
+-- 2. Lista de atores com o número de filmes em que participaram
+
+SELECT 
+    a.first_name,
+    a.last_name,
+    COUNT(fa.film_id) AS quantidade_filmes
+FROM actor a
+INNER JOIN film_actor fa ON a.actor_id = fa.actor_id
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY quantidade_filmes DESC;
+
+
+-- 3. Lista de atores que atuaram em filmes com mais de 120 minutos
+
+SELECT 
+    a.first_name,
+    a.last_name,
+    COUNT(f.film_id) AS quantidade_filmes
+FROM actor a
+INNER JOIN film_actor fa ON a.actor_id = fa.actor_id
+INNER JOIN film f ON fa.film_id = f.film_id
+WHERE f.length > 120
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY quantidade_filmes DESC;
